0c008b7a1880fbba149d441c622c64076de5d528
Valokoodari <valokoodari@gmail.com>
2019-12-10 13:06:39 +0200

    Refactoring of day 10 C++.

--- day-10/10.cpp
+++ day-10/10.cpp
@@ -1,7 +1,6 @@
 #define _USE_MATH_DEFINES
 
 #include <algorithm>
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -10,10 +9,9 @@
 typedef std::vector<std::vector<int> > intVec2D;
 
 intVec2D readFile() {
-    intVec2D map;
-    
     std::fstream file("10-input", std::fstream::in);
 
+    intVec2D map;
     std::string line;
     while (std::getline(file, line)) {
         map.push_back(std::vector<int>(0));
@@ -33,29 +31,25 @@ void writeFile(int a, int b) {
     file.close();
 }
 
-int count(int x, int y, intVec2D map) {
+int countDetects(int x, int y, intVec2D map) {
     intVec2D scanner(map.size(), std::vector<int>(map[0].size(), 0));
     int count = 0;
 
     for (int i = -30; i <= 30; i++) {
         for (int j = -30; j <= 30; j++) {
-            if (i == 0 && j == 0) {
+            if (i == 0 && j == 0)
                 continue;
-            }
-            if (x+i < 0 || y+j < 0 || x+i >= map.size() || y+j >= map[x].size())
+            if (y+i < 0 || x+j < 0 || y+i >= map.size() || x+j >= map[y].size())
                 continue;
 
-            if (map[x+i][y+j] != 0) {
+            if (map[y+i][x+j] != 0) {
                 int gcd;
                 if (j == 0)
-                    gcd = std::__algo_gcd(j,i);
+                    gcd = abs(std::__algo_gcd(j,i));
                 else
-                    gcd = std::__algo_gcd(i,j);
-                gcd = abs(gcd);
-                int di = i/gcd;
-                int dj = j/gcd;
-                if (scanner[x+di][y+dj] == 0) {
-                    scanner[x+di][y+dj]++;
+                    gcd = abs(std::__algo_gcd(i,j));
+                if (scanner[y+i/gcd][x+j/gcd] == 0) {
+                    scanner[y+i/gcd][x+j/gcd]++;
                     count++;
                 }
             }
@@ -66,85 +60,79 @@ int count(int x, int y, intVec2D map) {
 }
 
 std::vector<int> solveA(intVec2D map) {
-    for (int i = 0; i < map.size(); i++) {
-        for (int j = 0; j < map[i].size(); j++) {
-            if (map[i][j] == -1)
-                map[i][j] = count(i, j, map);
-        }
-    }
+    std::vector<int> data(3,0); // detects, x, y
 
-    int max = 0;
-    int x = 0;
-    int y = 0;
     for (int i = 0; i < map.size(); i++) {
         for (int j = 0; j < map[i].size(); j++) {
-            if (map[i][j] > max) {
-                max = map[i][j];
-                x = i;
-                y = j;
+            if (map[i][j] != 0) {
+                int detects = countDetects(j, i, map);
+                if (detects > data[0]) {
+                    data[0] = detects;
+                    data[1] = j;
+                    data[2] = i;
+                }
             }
         }
     }
 
-    return std::vector<int>{ max, x, y };
+    return data;
 }
 
 int solveB(intVec2D map, int x, int y) {
-    std::vector<std::vector<double> > angles;
+    std::vector<std::vector<double> > asteroids; // angle, distance, x, y
 
     for (int i = 0; i < map.size(); i++) {
         for (int j = 0; j < map[i].size(); j++) {
             if (j == x && i == y)
                 continue;
             if (map[i][j] != 0) {
-                double length = std::sqrt((j-x)*(j-x)+(i-y)*(i-y));
-                double angle = std::atan2((j-x),(i-y)) * 180 / M_PI - 180;
-                if (angle < 0) {
-                    angle = abs(angle);
-                }
-                angles.push_back(std::vector<double>{ angle, length, (double)j, (double)i });
+                double distance = std::sqrt((j-x)*(j-x) + (i-y)*(i-y));
+                double angle = abs(std::atan2((j-x),(i-y)) * 180 / M_PI - 180);
+                asteroids.push_back(std::vector<double>{ angle, distance, (double)j, (double)i });
             }
         }
     }
 
-    int destroyed = 0;
-    int px = 0;
-    int py = 0;
-    double laser = -0.000000001;
-    while (destroyed < 200) {
-        double small = 360;
-        int si = 0;
-        for (int i = 0; i < angles.size(); i++) {
-            if (angles[i][0] > laser) {
-                if (angles[i][0] < small) {
-                    small = angles[i][0];
-                    si = i;
-                } else if (abs(angles[i][0] - angles[si][0]) < 0.0001) {
-                    if (angles[i][1] < angles[si][1]) {
-                        si = i;
+    std::vector<int> data(3,0); // destroyed, x, y
+    double laserAngle = -0.000000001;
+
+    while (data[0] < 200) {
+        double smallest = 360;
+        int indexSmallest = 0;
+
+        for (int i = 0; i < asteroids.size(); i++) {
+            if (asteroids[i][0] > laserAngle) {
+                if (asteroids[i][0] < smallest) {
+                    smallest = asteroids[i][0];
+                    indexSmallest = i;
+                } else if (asteroids[i][0] == asteroids[indexSmallest][0]) {
+                    if (asteroids[i][1] < asteroids[indexSmallest][1]) {
+                        indexSmallest = i;
                     }
                 }
             }
         }
-        if (small == 360) {
-            laser = -0.000000001;
+
+        if (smallest == 360) {
+            laserAngle = -0.000000001;
             continue;
         }
-        px = angles[si][2];
-        py = angles[si][3];
-        laser = angles[si][0];
-        angles.erase(angles.begin() + si);
-        destroyed++;
+
+        data[1] = asteroids[indexSmallest][2];
+        data[2] = asteroids[indexSmallest][3];
+        laserAngle = asteroids[indexSmallest][0];
+        asteroids.erase(asteroids.begin() + indexSmallest);
+        data[0]++;
     }
 
-    return 100*px+py;
+    return 100 * data[1] + data[2];
 }
 
 int main() {
     intVec2D map = readFile();
 
     std::vector<int> solA = solveA(map);
-    int solB = solveB(map, solA[2], solA[1]);
+    int solB = solveB(map, solA[1], solA[2]);
 
     writeFile(solA[0], solB);
 
