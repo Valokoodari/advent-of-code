e838ae81bf96a1992887fc2fb17270b7bc0428f5
Valokoodari <valokoodari@gmail.com>
2019-12-13 19:02:14 +0200

    Upgraded day 5.

--- day-05/5.cpp
+++ day-05/5.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include "../ICC/intCodeComputer.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
-std::vector<int> readFile() {
-    std::vector<int> code;
+typedef long long int ll;
+
+std::vector<ll> readFile() {
+    std::vector<ll> code;
     std::string input;
     std::cin >> input;
 
     while (input.length() > 0) {
         int c = input.find(",");
         if (c == -1) { // There is no comma at the end of the input
-            code.push_back(std::stoi(input.substr(0, input.length())));
+            code.push_back(std::stoll(input.substr(0, input.length())));
             break;
         }
-        code.push_back(std::stoi(input.substr(0, c)));
+        code.push_back(std::stoll(input.substr(0, c)));
         input.erase(0, c+1);
     }
 
@@ -25,81 +28,38 @@ void writeFile(int a, int b) {
     std::cout << "Part 2: " << b;
 }
 
-std::vector<int> getModes(std::string ins) {
-    std::vector<int> modes;
-
-    if (ins.size() >= 5) 
-        modes.push_back(std::stoi(ins.substr(2,1)));
-    if (ins.size() >= 4)
-        modes.push_back(std::stoi(ins.substr(1,1)));
-    if (ins.size() >= 3)
-        modes.push_back(std::stoi(ins.substr(0,1)));
-    while (modes.size() < 3)
-        modes.push_back(0);
-
-    return modes;
-}
-
-std::vector<int> getParameters(std::vector<int> code, int counter) {
-    std::vector<int> parameters;
-    std::vector<int> modes = getModes(std::to_string(code[counter]));
-
-    parameters.push_back(code[(modes[0] == 0)? code[counter+1] : counter+1]);
-    parameters.push_back(code[(modes[1] == 0)? code[counter+2] : counter+2]);
-    parameters.push_back(code[counter+3]);
-
-    return parameters;
-}
-
-std::vector<int> intCodeComputer(std::vector<int> code, int input) {
-    std::vector<int> output;
-    int counter = 0;
-    while (code[counter] != 99) {
-        std::string ins = std::to_string(code[counter]);
-        std::vector<int> params = getParameters(code, counter);
-        int opCode = std::stoi((ins.size() > 1) ? ins.substr(ins.size() - 2, 2) : ins);
-
-        if (opCode == 1) { // ADD
-            code[params[2]] = params[0] + params[1];
-            counter += 4;
-        } else if (opCode == 2) { // MUL
-            code[params[2]] = params[0] * params[1];
-            counter += 4;
-        } else if (opCode == 3) { // INPUT
-            code[code[counter+1]] = input;
-            counter += 2;
-        } else if (opCode == 4) { // OUTPUT
-            output.push_back(code[code[counter+1]]);
-            counter += 2;
-        } else if (opCode == 5) { // JUMP IF TRUE (NOT ZERO)
-            counter = (params[0] != 0)? params[1] : counter+3;
-        } else if (opCode == 6) { // JUMP IF FALSE (ZERO)
-            counter = (params[0] == 0)? params[1] : counter+3;
-        } else if (opCode == 7) { // IF LESS THAN
-            code[params[2]] = (params[0] < params[1])? 1 : 0;
-            counter += 4;
-        } else if (opCode == 8) { // IF EQUAL TO
-            code[params[2]] = (params[0] == params[1])? 1 : 0;
-            counter += 4;
-        } else { // ERROR
-            break;
-        }
-    }
-
-    return output;
-} 
-
 int main() {
     freopen("5-input", "r", stdin);
     freopen("5-output", "w", stdout);
 
-    std::vector<int> code = readFile();
+    std::vector<ll> code = readFile();
 
-    std::vector<int> test1 = intCodeComputer(code, 1);
-    std::vector<int> test5 = intCodeComputer(code, 5);
+    ll test1 = 0;
+    int prevOp = 0;
+    intCodeComputer comp1(code);
+    comp1.addInput(1);
+    
+    while (prevOp != 99) {
+        prevOp = comp1.step();
 
-    int solA = test1.back();
-    int solB = test5.back();
+        if (prevOp == 4)
+            test1 = comp1.getOutput();
+    }
+
+    ll test5 = 0;
+    prevOp = 0;
+    intCodeComputer comp5(code);
+    comp5.addInput(5);
+
+    while (prevOp != 99) {
+        prevOp = comp5.step();
+
+        if (prevOp == 4)
+            test5 = comp5.getOutput();
+    }
+
+    int solA = test1;
+    int solB = test5;
 
     writeFile(solA, solB);
 
