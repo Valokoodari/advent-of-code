75752c5434875ed34fd28f8e9471c1a05f5c073e
Valokoodari <valokoodari@gmail.com>
2019-12-27 17:20:17 +0200

    Solved 2015 day 3

---
+++ 2015/03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+x,y,xs,ys,xr,yr,ha,hb = 0,0,0,0,0,0,{(0,0)},{(0,0)}
+d = {'^': (0,1), 'v': (0,-1), '>': (1,0), '<': (-1,0)}
+for i,c in enumerate(open("inputs/03.in").readline()):
+    x += d[c][0]
+    y += d[c][1]
+    ha.add((x,y))
+    if (i&1):
+        xs += d[c][0]
+        ys += d[c][1]
+        hb.add((xs,ys))
+    else:
+        xr += d[c][0]
+        yr += d[c][1]
+        hb.add((xr,yr))
+
+print(len(ha), len(hb))
