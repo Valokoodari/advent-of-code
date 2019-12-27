da2b14c78b302619d724fe8f68a6b603d6a38dd9
Valokoodari <valokoodari@gmail.com>
2019-12-27 16:09:39 +0200

    Solved 2015 day 2

---
+++ 2015/02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+p,r = 0,0
+for l in open("inputs/02.in").readlines():
+    a,b,c,*_ = sorted(map(int, l.split('x')))
+    p += 3*a*b + 2*a*c + 2*b*c
+    r += 2*a + 2*b + a*b*c
+    
+print(p, r)
