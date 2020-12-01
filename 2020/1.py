5bab869f6f3b876d7c26c5a5dce54d6ff9318120
Valokoodari <valokoodari@gmail.com>
2020-12-01 07:23:54 +0200

    Renamed lines to numbers

--- 2020/1.py
+++ 2020/1.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import itertools
 
-lines = list(map(int, open("inputs/1.in", "r").readlines()))
+numbers = list(map(int, open("inputs/1.in", "r").readlines()))
 
-for a,b in itertools.combinations(lines, 2):
+for a,b in itertools.combinations(numbers, 2):
     if (a + b == 2020):
         print("Part 1: " + str(a * b))
 
-for a,b,c in itertools.combinations(lines, 3):
+for a,b,c in itertools.combinations(numbers, 3):
     if (a + b + c == 2020):
         print("Part 2: " + str(a * b * c))
