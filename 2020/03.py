1fa08d6473cb2bbb0978ddc3d7b6536616af83ec
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-03 07:29:55 +0200

    2020 - Day 3

---
+++ 2020/03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import re
+import math
+
+ls = [re.split("", l)[1:-2] for l in open("inputs/03.in", "r").readlines()]
+
+def func(cp, rp):
+    a,c = 0,0
+    for i in range(0, len(ls), rp):
+        if ls[i][c] == "#": a += 1
+        c = (c + cp) % len(ls[i])
+    return a
+
+print("Part 1:", func(3, 1))
+print("Part 2:", math.prod(func(i, 1) for i in range(1,9,2)) * func(1, 2))
