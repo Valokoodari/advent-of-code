e0ef3b32c179ff45c65136921fdaa11eddd9e88e
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-04 21:03:36 +0200

    2020 day 4 part 2 to only use regex.

--- 2020/04.py
+++ 2020/04.py
@@ -3,13 +3,15 @@ import re
 
 ps = open("inputs/04.in", "r").read().split("\n\n")
 
-fs = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-es = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-
-def b(v, l, h):
-    if not v.isdigit():
-        return False
-    return l <= int(v) <= h
+fs = {
+    "hgt": "^((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)$",
+    "ecl": "^amb|blu|brn|gry|grn|hzl|oth$",
+    "byr": "^19[2-9][0-9]|200[0-2]$",
+    "iyr": "^201[0-9]|2020$",
+    "eyr": "^202[0-9]|2030$",
+    "hcl": "^#[0-9a-f]{6}$",
+    "pid": "^[0-9]{9}$"
+}
 
 def ca(s):
     for f in fs:
@@ -20,18 +22,8 @@ def dv(s):
     if not ca(s): return False
     p = re.split("[:\n ]", s)
 
-    if not b(p[p.index("byr")+1], 1920, 2002): return False
-    if not b(p[p.index("iyr")+1], 2010, 2020): return False
-    if not b(p[p.index("eyr")+1], 2020, 2030): return False
-
-    v = p[p.index("hgt")+1]
-    if v[-2:] == "cm" and not b(v[:-2], 150, 193): return False
-    if v[-2:] == "in" and not b(v[:-2], 59, 76): return False
-    if v[-2:] not in ["cm", "in"]: return False
-
-    if not re.search("^#[0-9a-f]{6}$", p[p.index("hcl")+1]): return False
-    if p[p.index("ecl")+1] not in es:  return False
-    if not re.search("^[0-9]{9}$", p[p.index("pid")+1]): return False
+    for f in fs:
+        if not re.match(fs[f], p[p.index(f)+1]): return False
 
     return True
 
