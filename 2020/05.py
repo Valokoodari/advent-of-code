062e3de9e8da4ba91be36c59b87d02e89e3a83d5
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-05 08:19:47 +0200

    2020 day 5 cleanup

--- 2020/05.py
+++ 2020/05.py
@@ -3,11 +3,11 @@ ls = [l.strip() for l in open("inputs/05.in", "r").readlines()]
 
 tb = { "F": "0", "B": "1", "L": "0", "R": "1" }
 
-ss,a = [],0
+ss = []
 for l in ls:
     for k,v in tb.items():
         l = l.replace(k, v)
-    ss.append(int(l[:-3],2)*8 + int(l[7:], 2))
+    ss.append(int(l[:-3],2)*8 + int(l[7:],2))
 print("Part 1:", max(ss))
 
 ss.sort()
