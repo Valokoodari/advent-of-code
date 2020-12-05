240581104aaa3cdf0b289fa49e2b1c193716d726
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-05 08:14:13 +0200

    2020 day 4 part 2 even more regex

--- 2020/04.py
+++ 2020/04.py
@@ -4,13 +4,13 @@ import re
 ps = open("inputs/04.in", "r").read().split("\n\n")
 
 fs = {
-    "hgt": "^((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)$",
-    "ecl": "^amb|blu|brn|gry|grn|hzl|oth$",
-    "byr": "^19[2-9][0-9]|200[0-2]$",
-    "iyr": "^201[0-9]|2020$",
-    "eyr": "^202[0-9]|2030$",
-    "hcl": "^#[0-9a-f]{6}$",
-    "pid": "^[0-9]{9}$"
+    "hgt": "(((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in))",
+    "ecl": "(amb|blu|brn|gry|grn|hzl|oth)",
+    "byr": "(19[2-9][0-9]|200[0-2])",
+    "iyr": "(201[0-9]|2020)",
+    "eyr": "(202[0-9]|2030)",
+    "hcl": "#[0-9a-f]{6}",
+    "pid": "[0-9]{9}"
 }
 
 def ca(s):
@@ -19,12 +19,8 @@ def ca(s):
     return True
 
 def dv(s):
-    if not ca(s): return False
-    p = re.split("[:\n ]", s)
-
-    for f in fs:
-        if not re.match(fs[f], p[p.index(f)+1]): return False
-
+    for k,v in fs.items():
+        if not re.fullmatch("^.*"+k+":"+v+"([ \n].*|)$", s, re.DOTALL): return False
     return True
 
 print("Part 1:", sum(ca(p) for p in ps))
