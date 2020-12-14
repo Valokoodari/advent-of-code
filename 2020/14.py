e2c845607ac284b80e55472f0f7b1c97533c1112
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-14 09:50:15 +0200

    2020 day 14 slight optimization

--- 2020/14.py
+++ 2020/14.py
@@ -3,14 +3,16 @@ import re
 
 ls = [re.split(" = ", l.strip()) for l in open("inputs/14.in", "r").readlines()]
 
-mem1,mem2 = {},{}
+mem1,mem2,b = {},{},36
 
-def w2(m, a, v):
+def w2(a, v):
     ar = [""]
-    for i,c in enumerate(m):
-        ar = [ad+a[i] if c == "0" else ad+"1" for ad in ar]
+    for i,c in enumerate(a):
         if c == "X":
-            ar += [ad[:-1]+"0" for ad in ar]
+            ar = [ad+"0" for ad in ar]
+            ar += [ad[:-1]+"1" for ad in ar]
+        else:
+            ar = [ad+a[i] for ad in ar]
     for ad in ar: mem2[int(ad,2)] = v
 
 m = "X"*36
@@ -19,11 +21,11 @@ for l in ls:
         m = l[1]
     else:
         l = [str(bin(int(l[0][4:][:-1])))[2:], str(bin(int(l[1])))[2:]]
-        v = (36-len(l[1]))*"0" + l[1]
-        a = (36-len(l[0]))*"0" + l[0]
+        v = (b-len(l[1]))*"0" + l[1]
+        a = (b-len(l[0]))*"0" + l[0]
 
         mem1[int(a,2)] = int("".join([v[i] if m[i] == "X" else m[i] for i in range(len(v))]), 2)
-        w2(m, a, int(v,2))
+        w2("".join([a[i] if m[i] == "0" else m[i] for i in range(b)]), int(v,2))
 
 print("Part 1:", sum([v for v in mem1.values()]))
 print("Part 2:", sum([v for v in mem2.values()]))
