ff465dbe73daef61da22422deb82caca39259546
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-15 09:41:00 +0200

    2020 day 15 from 36.680 s to 16.485 s

--- 2020/15.py
+++ 2020/15.py
@@ -1,18 +1,11 @@
 #!venv/bin/python3
-turns = {int(n): [i] for i,n in enumerate(open("inputs/15.in", "r").readline().split(","))}
+ts = {int(n): i for i,n in enumerate(open("inputs/15.in", "r").readline().split(","))}
 
-a1,l = 0,[k for k in turns][-1]
-for i in range(len(turns), 30000000):
-    a = 0
-    if len(turns[l]) >= 2:
-        a = i - 1 - turns[l][-2]
-    if a not in turns:
-        turns[a] = [i]
-    else:
-        turns[a].append(i)
-    l = a
-    if i+1 == 2020:
-        a1 = l
+a1,l = 0,ts.popitem()[0]
+for i in range(len(ts), 30000000-1):
+    a = i - ts[l] if l in ts else 0
+    ts[l],l = i,a
+    if i+2 == 2020: a1 = a
 
 print("Part 1:", a1)
 print("Part 2:", l)
