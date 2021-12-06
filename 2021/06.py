dd8e58b0a47fbbbae4fd026fee9942bbf5b7c5b1
Valokoodari <Valokoodari@users.noreply.github.com>
2021-12-06 08:54:57 +0200

    2021 - Day 6, Python

---
+++ 2021/06.py
@@ -0,0 +1,9 @@
+fs = [open("inputs/06.in").read().count(str(i)) for i in range(9)]
+
+for _ in range(80):
+    fs = [fs[i%9] if i != 7 else fs[i%9]+fs[0] for i in range(1,10)]
+print(f"Part 1: {sum(fs)}")
+
+for _ in range(256-80):
+    fs = [fs[i%9] if i != 7 else fs[i%9]+fs[0] for i in range(1,10)]
+print(f"Part 2: {sum(fs)}")
