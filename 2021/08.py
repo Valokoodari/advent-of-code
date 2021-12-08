956151ec363a7da107a914f1f739ad00076d9d62
Valokoodari <Valokoodari@users.noreply.github.com>
2021-12-08 09:26:37 +0200

    2021 - Day 8, Python : slightly cleaner

--- 2021/08.py
+++ 2021/08.py
@@ -4,19 +4,15 @@ ls = [([tuple(sorted([c for c in n])) for n in p.split(" ")], [tuple(sorted([c f
 s, sd = 0, { 2: 1, 4: 4, 3: 7, 7: 8 }
 for p,o in ls:
     ns = { sd[len(n)]: n for n in p if len(n) in sd }
-    p = [n for n in p if n not in ns.values()]
 
     ns[3] = [n for n in p if len(n) == 5 and len([c for c in n if c in ns[1]]) == 2][0]
-    p.remove(ns[3])
+    ns[6] = [n for n in p if len(n) == 6 and len([c for c in n if c in ns[7]]) == 2][0]
+    ns[9] = [n for n in p if len(n) == 6 and len([c for c in n if c in ns[4]]) == 4][0]
+
+    p = [n for n in p if n not in ns.values()]
     ns[5] = [n for n in p if len(n) == 5 and len([c for c in n if c in ns[4]]) == 3][0]
-    p.remove(ns[5])
-    ns[2] = [n for n in p if len(n) == 5][0]
-    p.remove(ns[2])
-    ns[6] = [n for n in p if len([c for c in n if c in ns[1]]) == 1][0]
-    p.remove(ns[6])
-    ns[9] = [n for n in p if len([c for c in n if c in ns[5]]) == 5][0]
-    p.remove(ns[9])
-    ns[0] = p[0]
+    ns[2] = [n for n in p if len(n) == 5 and len([c for c in n if c in ns[4]]) == 2][0]
+    ns[0] = [n for n in p if len(n) == 6][0]
 
     ns = { n: cs for cs,n in ns.items() }
     s += int("".join([str(ns[n]) if n in ns else "0" for n in o]))
