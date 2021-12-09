8f92eb7d9ca219daa896c099887ae8a221a4028e
Valokoodari <Valokoodari@users.noreply.github.com>
2021-12-09 18:18:42 +0200

    2015 - Day 9, Python : (written last year?)

---
+++ 2015/09.py
@@ -0,0 +1,26 @@
+p1,p2,ps,ds = int(1e12),0,[],{}
+
+for l in open("inputs/09.in").readlines():
+    a,_,b,_,d = l.split()
+    if a not in ps:
+        ps.append(a)
+        ds[a] = {}
+    if b not in ps:
+        ps.append(b)
+        ds[b] = {}
+    ds[a][b],ds[b][a] = int(d),int(d)
+
+def search(c, ps, d):
+    global p1,p2
+    ps.remove(c)
+    if len(ps) == 0:
+        if d < p1: p1 = d
+        if d > p2: p2 = d
+        return
+    for p in ps:
+        search(p, ps[:], d + ds[c][p])
+
+for p in ps:
+    search(p, ps[:], 0)
+
+print(f"Part 1: {p1}\nPart 2: {p2}")
