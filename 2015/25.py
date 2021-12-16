f9f2382300d2492cb9f9e1fc35737ebb195648e1
Valokoodari <Valokoodari@users.noreply.github.com>
2021-12-16 12:33:04 +0200

    2015 - Day 25, Python

---
+++ 2015/25.py
@@ -0,0 +1,9 @@
+r,c = open("inputs/25.in").read().strip().split(" row ")[1].split(", column ")
+r,c = int(r), int(c[:-1])
+n = sum(range(1,c+1)) + sum(range(c, r+c-1))
+
+a = 20151125
+for _ in range(n-1):
+    a = (a*252533) % 33554393
+
+print(f"Code: {a}")
