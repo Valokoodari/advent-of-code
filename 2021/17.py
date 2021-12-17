f42b999d3c942e814b5afa690436113a3e7416d2
Valokoodari <Valokoodari@users.noreply.github.com>
2021-12-17 08:55:33 +0200

    2021 - Day 17, Python : 195 ms -> 100 ms

--- 2021/17.py
+++ 2021/17.py
@@ -2,7 +2,7 @@ ls = [l.strip() for l in open("inputs/17.txt").read().strip().splitlines()]
 ta = [tuple(map(int, l.strip()[2:].split(".."))) for l in ls[0][13:].split(",")]
 
 a2 = 0
-for ivx in range(ta[0][1]+1):
+for ivx in range(int((ta[0][0]*2)**0.5), ta[0][1]+1):
     for ivy in range(ta[1][0],-ta[1][0]+1):
         pos, vx, vy = [0, 0], ivx, ivy
         while True:
--- 2021/README.md
+++ 2021/README.md
@@ -47,10 +47,10 @@ Day  Python  Rust
  14      27
  15     693   413
  16      27
- 17     195
+ 17     100
 -----------------
-Sum    2245   550
-Avg     132    69
+Sum    2150   550
+Avg     126    69
 ```
 
 Execution time is measured with `Measure-Command` in PowerShell 7 on Windows 11
