b9e314611d7ef875348dab6a823dd8e6e10f2f2a
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-03 08:05:12 +0200

    2022 - Updated template

--- 2022/solutions/_template.py
+++ 2022/solutions/_template.py
@@ -6,5 +6,13 @@ def part_2(data):
     return -1
 
 
+EX_1 = """\
+"""
+
 def test():
-    assert(False)
+    assert(part_1(EX_1) == 0)
+    assert(part_2(EX_1) == 0)
+
+
+if __name__ == "__main__":
+    test()
