5d252fe5852249a031922cd9820854bc4addc851
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-04 07:47:25 +0200

    2022 - Day 3, unnecessary brackets

--- 2022/solutions/day_03.py
+++ 2022/solutions/day_03.py
@@ -5,12 +5,12 @@ fp = lambda c: ord(c) - (38 if c.isupper() else 96)
 ps = lambda x: sum(fp(reduce(lambda a, b: set(a) & set(b), s).pop()) for s in x)
 
 def part_1(data):
-    return ps([(l[:len(l)//2], l[len(l)//2:]) for l in data.splitlines()])
+    return ps((l[:len(l)//2], l[len(l)//2:]) for l in data.splitlines())
 
 def part_2(data):
     ls = data.splitlines()
 
-    return ps([(ls[i], ls[i+1], ls[i+2]) for i in range(0, len(ls), 3)])
+    return ps((ls[i], ls[i+1], ls[i+2]) for i in range(0, len(ls), 3))
 
 
 EX_1 = """\
