fd8587201634dd1d1dba035e4bc03e128e7a9743
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-04 08:34:44 +0200

    2016 - Day 9, fix main script and add input

--- 2016/main.py
+++ 2016/main.py
@@ -3,7 +3,7 @@ from time import process_time
 import sys
 
 from solutions import day_01, day_02, day_03, day_04, day_05, day_06, day_07, \
-    day_08
+    day_08, day_09
 
 total_time = 0
 NAMES = (
@@ -15,6 +15,7 @@ NAMES = (
     "Signals and Noise",
     "Internet Protocol Version 7",
     "Two-Factor Authentication",
+    "Explosives in Cyberspace",
 )
 
 
