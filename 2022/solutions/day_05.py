3b938320571885a36676d153e1febcfeb18a636c
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-05 08:30:52 +0200

    2022 - Day 5, Even less lines

--- 2022/solutions/day_05.py
+++ 2022/solutions/day_05.py
@@ -1,9 +1,7 @@
-ps = lambda x, y: (["".join(s).strip() for s in list(zip(*x[-2::-1]))[1::4]], [[*map(int, l.split()[1::2])] for l in y])
-parse = lambda x: ps(*(p.splitlines() for p in x.split("\n\n")))
-
+parse = lambda x, y: (["".join(s).strip() for s in list(zip(*x[-2::-1]))[1::4]], [[*map(int, l.split()[1::2])] for l in y])
 
 def solve(data, r):
-    ss, os = parse(data)
+    ss, os = parse(*(p.splitlines() for p in data.split("\n\n")))
 
     for o in os:
         ss[o[1]-1], ss[o[2]-1] = ss[o[1]-1][:-o[0]], ss[o[2]-1] + ss[o[1]-1][-o[0]:][::r]
@@ -30,8 +28,8 @@ move 2 from 2 to 1
 move 1 from 1 to 2"""
 
 def test():
-    assert(part_1(EX_1) == "CMZ")
-    assert(part_2(EX_1) == "MCD")
+    assert part_1(EX_1) == "CMZ"
+    assert part_2(EX_1) == "MCD"
 
 
 if __name__ == "__main__":
