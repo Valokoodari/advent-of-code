269527b4fa2e36c791e2b0e5bcd61c6928cba425
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-11 09:48:44 +0200

    2022 - Template: do not test with empty input

--- 2022/solutions/_template.py
+++ 2022/solutions/_template.py
@@ -10,8 +10,9 @@ EX_0 = """\
 """
 
 def test():
-    assert part_1(EX_0) == 0
-    assert part_2(EX_0) == 0
+    assert EX_0 != "\n"
+    # assert part_1(EX_0) == 0
+    # assert part_2(EX_0) == 0
 
 
 if __name__ == "__main__":
