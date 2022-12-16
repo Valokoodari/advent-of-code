d8f13e819c82a64c148c048e861aa330c7ce59e5
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-16 11:11:50 +0200

    2022 - Day 16, just stats

--- 2022/main.py
+++ 2022/main.py
@@ -3,7 +3,7 @@ from time import process_time
 import sys
 
 from solutions import day_01, day_02, day_03, day_04, day_05, day_06, day_07, \
-    day_08, day_09, day_10, day_11, day_12, day_13, day_14, day_15
+    day_08, day_09, day_10, day_11, day_12, day_13, day_14, day_15, day_16
 
 
 total_time = 0
@@ -23,6 +23,7 @@ NAMES = (
     "Distress Signal",
     "Regolith Reservoir",
     "Beacon Exclusion Zone",
+    "Proboscidea Volcanium",
 )
 
 
