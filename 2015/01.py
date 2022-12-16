f30246f061dd6717af0a1c396508398ce259cf03
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-16 11:31:47 +0200

    2015 - Day 1: preparing to remove the inputs

--- 2015/01.py
+++ 2015/01.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 f,b,i = 0,0,1
-for c in open("inputs/01.in", 'r').readline():
+for c in open("../inputs/2015/01.txt", 'r').readline():
     if (c == '('): f += 1
     else: f -= 1
     if (f == -1 and b == 0): b = i
