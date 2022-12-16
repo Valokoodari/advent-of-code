e8e4a43e5119da11bd159a50eb1bfde5e86fab2a
Valokoodari <Valokoodari@users.noreply.github.com>
2022-12-16 11:32:55 +0200

    2015 - Day 2: preparing to remove the inputs

--- 2015/02.py
+++ 2015/02.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 p,r = 0,0
-for l in open("inputs/02.in").readlines():
+for l in open("../inputs/2015/02.txt").readlines():
     a,b,c,*_ = sorted(map(int, l.split('x')))
     p += 3*a*b + 2*a*c + 2*b*c
     r += 2*a + 2*b + a*b*c
