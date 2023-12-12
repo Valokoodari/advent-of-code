cb565e5596d3842930b1c6dd89f9831dc687ed75
Valokoodari <Valokoodari@users.noreply.github.com>
2023-12-12 10:19:57 +0200

    2023 - Day 5, -3 lines, and neater parser

--- 2023/solutions/day_05.py
+++ 2023/solutions/day_05.py
@@ -1,23 +1,20 @@
 def parse(d):
-    ps = d.split("\n\n")
-    ss = [int(x) for x in ps[0].split(": ")[1].split()]
-    ms = [[[int(x) for x in l.split()] for l in p.splitlines()[1:]] for p in ps[1:]]
+    ps = [p.splitlines() for p in d.split("\n\n")]
+    ss = [int(x) for x in ps[0][0].split(": ")[1].split()]
+    ms = [[[int(x) for x in l.split()] for l in p[1:]] for p in ps[1:]]
     return ss, ms
 
 
 def part_1(data):
     ss, ms = parse(data)
-
-    ns = []
-    for s in ss:
+    for i in range(len(ss)):
         for m in ms:
             for v in m:
-                if v[1] <= s < v[1] + v[2]:
-                    s += v[0] - v[1]
+                if v[1] <= ss[i] < v[1] + v[2]:
+                    ss[i] += v[0] - v[1]
                     break
-        ns.append(s)
 
-    return min(ns)
+    return min(ss)
 
 
 def part_2(data):
@@ -29,7 +26,7 @@ def part_2(data):
         for v in p:
             crs = []
             for r in rs:
-                if not (r[0] > v[1] + v[2] - 1 or r[1] < v[1]):
+                if not (r[0] >= v[1] + v[2] or r[1] < v[1]):
                     a, b = max(v[1], r[0]), min(v[1] + v[2] - 1, r[1])
                     nrs.append((a + v[0] - v[1], b + v[0] - v[1]))
                     if r[0] < a:
