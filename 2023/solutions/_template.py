77b0d68b40c091e1017c1bfcf8671b43d3e2da4c
Valokoodari <Valokoodari@users.noreply.github.com>
2023-12-16 06:34:25 +0200

    2023 - Template: return 0 by default

--- 2023/solutions/_template.py
+++ 2023/solutions/_template.py
@@ -1,9 +1,11 @@
 def part_1(data):
-    return -1
+    ans = 0
+    return ans
 
 
 def part_2(data):
-    return -1
+    ans = 0
+    return ans
 
 
 EX_0 = """\
@@ -11,8 +13,8 @@ EX_0 = """\
 
 def test():
     assert EX_0 != ""
-    assert part_1(EX_0) == 0
-    # assert part_2(EX_0) == 0
+    assert part_1(EX_0) == -1
+    # assert part_2(EX_0) == -1
 
 
 if __name__ == "__main__":
