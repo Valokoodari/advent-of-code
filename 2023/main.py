4729307494381d15a7924640a16cd425f7cc44f7
Valokoodari <Valokoodari@users.noreply.github.com>
2023-12-17 08:10:49 +0200

    2023 - Template: benchmark option

--- 2023/main.py
+++ 2023/main.py
@@ -89,6 +89,12 @@ def main():
         for day in range(len(NAMES)):
             if day+1 not in days:
                 run(day+1)
+    elif sys.argv[1] in ("-b", "--benchmark"):
+        day = [int(day) for day in sys.argv[2:] if day.isdigit()][0]
+        for _ in range(10):
+            run(day)
+        print(f"Average time: {colored(f'{total_time*100:.3f}', 'magenta')} ms")
+        exit()
     else:
         print(colored("Invalid arguments.\n", "red"))
 
