3e228b09f25674a099aa9feecef4ab2fbe2e40a5
Valokoodari <Valokoodari@users.noreply.github.com>
2023-12-20 13:14:34 +0200

    2023 - Day 19: Examples to files

--- 2023/solutions/day_19.py
+++ 2023/solutions/day_19.py
@@ -63,31 +63,16 @@ def part_2(data):
     return ans
 
 
-EX_0 = """\
-px{a<2006:qkq,m>2090:A,rfg}
-pv{a>1716:R,A}
-lnx{m>1548:A,A}
-rfg{s<537:gd,x>2440:R,A}
-qs{s>3448:A,lnx}
-qkq{x<1416:A,crn}
-crn{x>2662:A,R}
-in{s<1351:px,qqz}
-qqz{s>2770:qs,m<1801:hdj,R}
-gd{a>3333:R,R}
-hdj{m>838:A,pv}
+def test(get_tests = None):
+    if not get_tests:
+        from solutions.test import get_tests
 
-{x=787,m=2655,a=1222,s=2876}
-{x=1679,m=44,a=2067,s=496}
-{x=2036,m=264,a=79,s=2244}
-{x=2461,m=1339,a=466,s=291}
-{x=2127,m=1623,a=2188,s=1013}
-"""
-
-def test():
-    assert EX_0 != ""
-    assert part_1(EX_0) == 19114
-    assert part_2(EX_0) == 167409079868000
+    for data, a1, a2 in get_tests(19):
+        assert data != None and a1 or a2
+        assert a1 == None or str(part_1(data)) == a1
+        assert a2 == None or str(part_2(data)) == a2
 
 
 if __name__ == "__main__":
-    test()
+    from test import get_tests
+    test(get_tests)
