07518dd1e1b22be8386ccf3f59c06d01da675590
Valokoodari <Valokoodari@users.noreply.github.com>
2024-12-10 10:39:52 +0200

    2024 - Day 10: Even smaller

--- 2024/solutions/day_10.py
+++ 2024/solutions/day_10.py
@@ -3,19 +3,16 @@ def solve(d, p = 1, ans = 0):
     ss = [(r, c) for r in range(len(ms)) for c in range(len(ms[0])) if ms[r][c] == 0]
 
     for r, c in ss: 
-        q, es, vs = [(r, c)], set(), [[False]*len(ms[0]) for _ in range(len(ms))]
+        q, vs = [(r, c)], [[False]*len(ms[0]) for _ in range(len(ms))]
         while q:
             r, c = q.pop()
-            vs[r][c] = True
+            vs[r][c] = p == 1
             if ms[r][c] == 9:
-                es.add((r, c))
-                ans += 1 if p == 2 else 0
+                ans += 1
                 continue
             for dr, dc in ((-1,0), (0,1), (1,0), (0,-1)):
-                nr, nc = r+dr, c+dc
-                if 0 <= nr < len(ms) and 0 <= nc < len(ms[0]) and ms[nr][nc] == ms[r][c]+1 and (not vs[nr][nc] or p == 2):
+                if 0 <= (nr := r+dr) < len(ms) and 0 <= (nc := c+dc) < len(ms[0]) and ms[nr][nc] == ms[r][c]+1 and not vs[nr][nc]:
                     q.append((nr, nc))
-        ans += len(es) if p == 1 else 0
     return ans
 
 
