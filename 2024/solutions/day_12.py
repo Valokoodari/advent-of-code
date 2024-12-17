6c2a6bc749941ac659e889b6a1fee0561d1f0b2c
Valokoodari <Valokoodari@users.noreply.github.com>
2024-12-17 12:46:56 +0200

    2024 - Day 12: Less indentation

--- 2024/solutions/day_12.py
+++ 2024/solutions/day_12.py
@@ -1,38 +1,36 @@
 from collections import defaultdict
+from itertools import product
 
 
 def solve(data, t = 1):
     ms, h, w = [[c for c in r] for r in data.splitlines()], len(data.splitlines()), len(data.splitlines()[0])
     ans, vs = 0, [[False] * w for _ in range(h)]
 
-    for r in range(h):
-        for c in range(w):
-            if vs[r][c]:
-                continue
-            a, p, s, q, vs[r][c], ps = 1, 0, 0, [(r, c)], True, defaultdict(set)
-            while q:
-                y, x = q.pop()
-                for dy, dx in (DS := ((-1,0), (0,1), (1,0), (0,-1))):
-                    if 0 <= (ny := y+dy) < h and 0 <= (nx := x+dx) < w and ms[ny][nx] == ms[y][x]:
-                        if vs[ny][nx]:
-                            continue
-                        vs[ny][nx], a = True, a+1
-                        q.append((ny, nx))
-                    else:
-                        p += 1
-                        if t == 2:
-                            ps[(dy, dx)].add((y, x))
+    for r, c in product(range(h), range(w)):
+        if vs[r][c]: continue
+        a, p, s, q, vs[r][c], ps = 1, 0, 0, [(r, c)], True, defaultdict(set)
+        while q:
+            y, x = q.pop()
+            for dy, dx in (DS := ((-1,0), (0,1), (1,0), (0,-1))):
+                if 0 <= (ny := y+dy) < h and 0 <= (nx := x+dx) < w and ms[ny][nx] == ms[y][x]:
+                    if vs[ny][nx]: continue
+                    vs[ny][nx], a = True, a+1
+                    q.append((ny, nx))
+                else:
+                    p += 1
+                    if t == 2:
+                        ps[(dy, dx)].add((y, x))
 
-            for ss in ps.values():
-                vp = set()
-                for d in ss:
-                    q, s = ([], s) if d in vp else ([d], s+1)
-                    while q:
-                        if (np := q.pop()) in vp:
-                            continue
-                        vp.add(np)
-                        q.extend((np[0]+dr, np[1]+dc) for dr, dc in DS if (np[0]+dr, np[1]+dc) in ss)
-            ans += a * (p if t == 1 else s)
+        for ss in ps.values():
+            vp = set()
+            for d in ss:
+                q, s = ([], s) if d in vp else ([d], s+1)
+                while q:
+                    if (np := q.pop()) in vp:
+                        continue
+                    vp.add(np)
+                    q.extend((np[0]+dr, np[1]+dc) for dr, dc in DS if (np[0]+dr, np[1]+dc) in ss)
+        ans += a * (p if t == 1 else s)
     return ans
 
 
